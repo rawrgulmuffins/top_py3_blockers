869c872994f9f054b45f85b9d646aa9c9df5db93
Alex Lord <alord@isilon.com>
2015-05-01 21:52:04 -0700

    Changed logging to be exceptions rather than errors. Added a CircularDependencyError Except and KeyboardInterrupt. Created write_results function.

--- top_python3_blockers.py
+++ top_python3_blockers.py
@@ -8,6 +8,7 @@ import logging
 
 import caniusepython3
 from caniusepython3.dependencies import blocking_dependencies
+from caniusepython3.dependencies import CircularDependencyError
 
 def chunk_list(source_list, chunk_size):
     """
@@ -19,11 +20,16 @@ def chunk_list(source_list, chunk_size):
     for index in range(0, len(source_list), chunk_size):
         yield source_list[index:index + chunk_size]
 
+def write_results(top_blockers):
+    with open('top_blockers', 'wt') as out_file:
+        pprint(top_blockers.most_common(), stream=out_file)
+
+
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     all_py_projects = list(caniusepython3.pypi.all_projects())
     py3_projects = caniusepython3.pypi.all_py3_projects()
-    blockers_counter = Counter()
+    top_blockers = Counter()
     for py_projects_sublist in chunk_list(all_py_projects, 50):
         all_blockers = None
         try:
@@ -35,17 +41,24 @@ if __name__ == "__main__":
             for blockers_subset in all_blockers:
                 for blocker in blockers_subset:
                     blocker_list.append(blocker)
-            blockers_counter.update(blocker_list)
+            top_blockers.update(blocker_list)
         except SyntaxError as error:
-            logger.error("{} throw on projects {}".format(
+            logger.exception("{} throw on projects {}".format(
                 error,
                 py_projects_sublist))
         except TypeError as error:
-            logger.error("{} throw on projects {}".format(
+            logger.exception("{} throw on projects {}".format(
                 error,
                 py_projects_sublist))
         except AttributeError as error:
-            logger.error("{} throw on projects {}".format(
+            logger.exception("{} throw on projects {}".format(
                 error,
                 py_projects_sublist))
-    pprint(blockers_counter.most_common())
+        except CircularDependencyError as error:
+            logger.exception("{} throw on projects {}".format(
+                error,
+                py_projects_sublist))
+        except KeyboardInterrupt:
+            write_results(top_blockers)
+            exit()
+    write_results(top_blockers)
